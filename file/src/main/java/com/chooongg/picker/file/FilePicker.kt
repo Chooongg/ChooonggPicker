7889b9e5e3417048807a58aa47fa589d3c9c97b5
Chooongg <chooongg@outlook.com>
2024-05-06 17:51:06 +0800

    Initial commit

--- file/src/main/java/com/chooongg/picker/file/FilePicker.kt
+++ file/src/main/java/com/chooongg/picker/file/FilePicker.kt
@@ -1,22 +1,30 @@
 package com.chooongg.picker.file
 
 import android.content.Context
+import android.view.View
 import androidx.annotation.IntDef
 import androidx.annotation.IntRange
+import androidx.annotation.StringRes
 import androidx.annotation.StyleRes
 import java.io.File
 
-object FilePicker {
-
-    const val MODE_PAGE = 0
-    const val MODE_BOTTOM_SHEET = 1
+class FilePicker {
 
     @IntDef(MODE_PAGE, MODE_BOTTOM_SHEET)
     annotation class FilePickerMode
 
-    fun with(context: Context): FilePickerBuilder {
-        Config.reset()
-        return FilePickerBuilder(context)
+    companion object {
+        const val MODE_PAGE = 0
+        const val MODE_BOTTOM_SHEET = 1
+
+        fun with(context: Context): FilePickerBuilder {
+            Config.reset()
+            return FilePickerBuilder(context, null)
+        }
+
+        fun with(targetView: View): FilePickerBuilder {
+            return FilePickerBuilder(targetView.context, targetView)
+        }
     }
 
     internal object Config {
@@ -29,21 +37,27 @@ object FilePicker {
 
         @IntRange(from = 1)
         var maxCount: Int = 1
+
+        @StringRes
+        var chooseButtonText: Int = 0
         var supportedTypes: List<String>? = null
         var excludedTypes: List<String>? = null
         var selectedFiles: List<File>? = null
+        var listener: ((MutableList<File>) -> Unit)? = null
 
         fun reset() {
             theme = 0
             mode = MODE_PAGE
             maxCount = 1
+            chooseButtonText = 0
             supportedTypes = null
             excludedTypes = null
             selectedFiles = null
+            listener = null
         }
     }
 
-    class FilePickerBuilder internal constructor(val context: Context) {
+    class FilePickerBuilder internal constructor(val context: Context, val targetView: View?) {
 
         fun theme(@StyleRes theme: Int) = apply {
             Config.theme = theme
@@ -67,6 +81,14 @@ object FilePicker {
 
         fun selectedFiles(files: List<File>) = apply {
             Config.selectedFiles = files
+        }
+
+        fun chooseButtonText(@StringRes id: Int) = apply {
+            Config.chooseButtonText = id
+        }
+
+        fun start(listener: (MutableList<File>) -> Unit) {
+
         }
     }
 }
--- file/src/main/java/com/chooongg/picker/file/viewModel/LastFileViewModel.kt
+++ file/src/main/java/com/chooongg/picker/file/viewModel/LastFileViewModel.kt
@@ -3,6 +3,9 @@ package com.chooongg.picker.file.viewModel
 import android.content.Context
 import android.net.Uri
 import android.os.FileUtils
+import android.provider.DocumentsContract
+import android.provider.DocumentsProvider
+import android.provider.MediaStore.Images.Media
 import androidx.core.content.FileProvider
 import androidx.lifecycle.ViewModel
 import java.io.File
@@ -10,8 +13,8 @@ import java.io.File
 class LastFileViewModel : ViewModel() {
 
     fun getLastFiles(context: Context) {
-        context.contentResolver.query(FileProvider.getUriForFile(), null, null, null, null)?.use {
-
+        context.contentResolver.query(Media.EXTERNAL_CONTENT_URI, null, null, null, null)?.use {
+            Media.EXTERNAL_CONTENT_URI
         }
     }
 }
